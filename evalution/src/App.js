e07c994cca3286e340a36098421280100b17cd8b
Tejaswini Mahale <tejaswinimahale11@gmail.com>
2022-05-02 17:13:21 +0530

    updates

---
+++ evalution/src/App.js
@@ -0,0 +1,20 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="div">
+      <h1>Home</h1>
+      <h1>About</h1>
+      <h1>Products</h1>
+      <h1>Women</h1>
+      <h1>Men</h1>
+      <h1>Kids</h1>
+      <h1>Home Decor</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
